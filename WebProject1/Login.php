cd30c97770c552cb33c26be91615888432263f4b
YuhanXue <35746251+YuhanXue@users.noreply.github.com>
2018-03-13 13:37:41 +0800

    Front-end work of Login Page
    To be continued. Pictures not added.

---
+++ WebProject1/Login.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>Login</title>
+    <link rel="stylesheet" href="Login.css">
+</head>
+
+<body>
+<div class="loginBox">
+    <img src="" class="user">
+    <h1 class="">User Login</h1>
+
+    <form>
+        <div id="div1">
+            <p>Username</p>
+            <input type="text" id="Username" placeholder="Username" name="username">
+        </div>
+
+        <div id="div2">
+            <p>Password</p>
+            <input type="password" id="password" placeholder="Password" name="password">
+        </div>
+
+        <div id="div3">
+            <input type="submit" name="" value="Sign In">
+        </div>
+
+        <a href="">Forget Your Password ?</a>
+    </form>
+
+</div>
+</body>
+</html>
