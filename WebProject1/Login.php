98744f578d09734013bb8a55dfdd35ada13fbcb1
YuhanXue <35746251+YuhanXue@users.noreply.github.com>
2018-03-16 07:50:18 -0400

    some changes.
    some changes.

--- WebProject1/Login.php
+++ WebProject1/Login.php
@@ -6,18 +6,18 @@
 
 <body>
 <div class="loginBox">
-    <img src="" class="user">
+    <img src="../WebProject1/includes/UserProfile.png" class="user">
     <h1 class="">User Login</h1>
 
     <form>
         <div id="div1">
             <p>Username</p>
-            <input type="text" id="Username" placeholder="Username" name="username">
+            <input type="text" id="Username" placeholder="Enter Username" name="username">
         </div>
 
         <div id="div2">
             <p>Password</p>
-            <input type="password" id="password" placeholder="Password" name="password">
+            <input type="password" id="password" placeholder="Enter Password" name="password">
         </div>
 
         <div id="div3">
