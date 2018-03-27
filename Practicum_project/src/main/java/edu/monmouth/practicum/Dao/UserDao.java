e17cf2bf6f0b71fcb14f3aff3693afde6b2d511f
chengli-Leo <s1209724@monmouth.edu>
2018-03-27 19:18:11 -0400

    change company review font-end

--- Practicum_project/src/main/java/edu/monmouth/practicum/Dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package edu.monmouth.practicum.Dao;
-
-import edu.monmouth.practicum.Domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User,Integer>{
-    User findByUsernameAndPassword(String username,String password);
-}
