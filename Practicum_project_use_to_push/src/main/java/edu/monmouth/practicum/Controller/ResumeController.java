b490df7da012fc42138803b700f661ef587ddb92
chengli-Leo <s1209724@monmouth.edu>
2018-11-06 15:37:32 -0500

    update fuction

--- Practicum_project_use_to_push/src/main/java/edu/monmouth/practicum/Controller/ResumeController.java
+++ Practicum_project_use_to_push/src/main/java/edu/monmouth/practicum/Controller/ResumeController.java
@@ -39,7 +39,38 @@ public class ResumeController {
                 byte[] buffer = new byte[1024];
                 FileInputStream fis = null;
                 BufferedInputStream bis = null;
-               
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                       // System.out.println("success-------------");
+                    }
+                    System.out.println("success");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
         return "Download_Successful";
     }
 
