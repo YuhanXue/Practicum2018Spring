4c121faad331f335a0d1df4855ed31d378b23890
chengli-Leo <s1209724@monmouth.edu>
2018-11-06 15:34:29 -0500

    update fuction

---
+++ Practicum_project_use_to_push/src/main/java/edu/monmouth/practicum/Controller/ResumeController.java
@@ -0,0 +1,31 @@
+package edu.monmouth.practicum.Controller;
+
+import edu.monmouth.practicum.Dao.ResumeDao;
+import edu.monmouth.practicum.Domain.Resume;
+import edu.monmouth.practicum.Domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.*;
+import java.util.List;
+
+@Controller
+public class ResumeController {
+    @Resource
+    public ResumeDao resumeDao;
+    @RequestMapping(value = "/download",method = RequestMethod.GET)
+    public String download_resume(@RequestParam("filename") String fileName, HttpSession session,HttpServletRequest request, HttpServletResponse response){
+       
+    }
+
+}
